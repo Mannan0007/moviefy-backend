6637e78639a45561a376b9c0fce29639419ba6dc
Mannan0007 <mauu0052@gmail.com>
2025-08-03 00:59:46 +0530

    backend update

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(express.json()) //to accept the json format data
 
 app.use(
   cors({
-    origin: "http://localhost:5173", // ✅ Allow frontend to access backend
+    origin: "http://localhost:5173" || "https://moviefy-frontend.vercel.app", // ✅ Allow frontend to access backend
     methods: "GET,POST,PUT,DELETE",
     credentials: true, // ✅ Allow cookies if needed
   })
